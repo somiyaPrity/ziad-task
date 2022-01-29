eb3c76f2b690419877537696627f7bae3da83716
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-01-30 01:39:58 +0600

    validation

--- js-file/script.js
+++ js-file/script.js
@@ -63,26 +63,33 @@ function validation() {
   let country  = document.getElementById('country');
   let countryName = document.getElementById('country_name');
   if (name.length == 0) {
+    document.getElementById('name_error').textContent = 'Please input a valid name';
     return false
   }
   if (email.length == 0) {
     let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let emailValidate = re.test(String(email).toLowerCase());
     if(emailValidate==false){
+      document.getElementById('email_error').textContent = 'Please input a valid email';
       return false;
     }
   }
   if (phone.length < 11 ) {
+    document.getElementById('phone_error').textContent = 'Please input a valid phone number';
     return false
   }
   if (genderMale == false && genderFemale == false) {
+    document.getElementById('gender_error').textContent = 'Please select gender';
     return false
   } 
-  if(country.value == null){
+  if(country.value == ''){
+    console.log(country.value);
+    document.getElementById('country_error').textContent = 'Please select your country';
     return false;
   }
   if(country.value==2){
     if(countryName.length==0){
+      document.getElementById('country_name_error').textContent = 'Please write your country name';
       return false;
     }
   }
