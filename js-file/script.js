d6b532490d20a90231d8563709391d57e6d725cb
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-01-29 23:42:17 +0600

    email validatipon by regex

--- js-file/script.js
+++ js-file/script.js
@@ -44,31 +44,57 @@ form.addEventListener('click', (e) => {
 })
 
 //form validation
+let country  = document.getElementById('country');
+let countryName = document.getElementById('country_name');
+country.addEventListener('change',()=>{
+  if(country.value==2){
+    countryName.removeAttribute('disabled');
+  }
+  if(country.value==1){
+    countryName.setAttribute('disabled',true);
+  }
+})
 function validation() {
   let name = document.getElementById('name').value
   let email = document.getElementById('email').value
   let phone = document.getElementById('phone').value
   let genderMale = document.getElementById('inlineRadio1').checked
   let genderFemale = document.getElementById('inlineRadio2').checked
+  let country  = document.getElementById('country');
+  let countryName = document.getElementById('country_name');
   if (name.length == 0) {
     return false
   }
   if (email.length == 0) {
-    return false
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let emailValidate = re.test(String(email).toLowerCase());
+    if(emailValidate==false){
+      return false;
+    }
   }
-  if (phone.length == 0) {
+  if (phone.length < 11 ) {
     return false
   }
   if (genderMale == false && genderFemale == false) {
     return false
-  } else {
+  } 
+  if(country.value == null){
+    return false;
+  }
+  if(country.value==2){
+    if(countryName.length==0){
+      return false;
+    }
+  }
+  else {
     return true
   }
 }
 
 document.getElementById('submit_btn').addEventListener('click', (e) => {
-  e.preventDefault()
+  e.preventDefault();
   if (!validation()) {
     return false
   }
+  console.log('validate successfull');
 })
